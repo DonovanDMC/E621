bddc7568c450d58b6bb516d57347692138af12f9
Donovan Daniels <hewwo@yiff.rocks>
2022-01-16 13:52:29 -0600

    make `debug` optional

--- src/util/Debug.ts
+++ src/util/Debug.ts
@@ -1,5 +1,11 @@
-import debug from "debug";
-
 export default function Debug(name: string, first: unknown, ...extra: Array<unknown>) {
+	// eslint-disable-next-line @typescript-eslint/consistent-type-imports
+	let debug: import("debug").Debug;
+	try {
+		// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+		debug = require("debug");
+	} catch {
+		return;
+	}
 	return debug(!name ? "e621:general" : `e621:${name}`)(first, ...extra);
 }
